c5e50a18e0f534ee026b3918c6b6d0d75a14eb46
Christian <cchaoayan@gmail.com>
2026-04-20 15:04:01 +0800

    added headless:true

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,6 +32,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://automationexercise.com/',
+    headless: true,
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
 
