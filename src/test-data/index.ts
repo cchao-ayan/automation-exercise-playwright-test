9527486d2bc93a93228da40ef6cfe786fbe72f18
Christian <cchaoayan@gmail.com>
2026-04-29 08:17:12 +0800

    remove filePath inside index

--- src/test-data/index.ts
+++ src/test-data/index.ts
@@ -2,13 +2,11 @@ import * as contactUsValid from './contact-us/valid.test-data.json';
 import * as contactUsInvalid from './contact-us/invalid.test-data.json';
 import * as products from './products/products.test-data.json';
 import * as signup from './signup/signup.test-data.json';
-import * as  filePath from './file-path/file-paths.json';
 // Add more as needed
 
 export {
     contactUsValid,
     contactUsInvalid,
     products,
-    signup,
-    filePath
+    signup
 }
